b8167b0db11c3e82afad7fe982a5b88855a72870
Stuart Palmer <palmersf@gmail.com>
2020-10-17 14:26:56 -0400

    completes initial functionality

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 
 
 // Constants and variables
-const API_KEY = '';
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const API_KEY = CONFIG.openWeatherAPIKey;
+
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast?q=';
 let weatherData, userInput;
 
 // Cached Element References
 const $title = $('#title');
+const $forecast = $('#forecast');
 const $temp = $('#temp');
 const $index = $('#index');
 const $desc = $('#desc');
 const $input = $('input[type="text"]');
 const $form = $('form');
+const $days = $('#days');
+
+
 
 // Event listeners
 $form.on('submit', handleGetData);
@@ -22,8 +27,11 @@ function handleGetData(event) {
     
     event.preventDefault();
     userInput = $input.val();
+    numDays = $days.val();
+    console.log(numDays);
 
     if(!userInput) return;
+
 
     $.ajax(BASE_URL + userInput + '&units=imperial&appid=' + API_KEY)
     .then(function(data) {
@@ -35,8 +43,8 @@ function handleGetData(event) {
 }
 
 function render() {
-    $title.text('Weather For: ' + weatherData.name);
-    $temp.text('Temperature: ' + weatherData.main.temp + 'º');
-    $index.text('Feels Like: ' + weatherData.main.feels_like + 'º');
-    $desc.text('Weather: ' + weatherData.weather[0].description);
+    $title.text('Weather For: ' + weatherData.city.name);
+    $temp.text('Temperature: ' + weatherData.list[0].main.temp + 'º');
+    $index.text('Feels Like: ' + weatherData.list[0].main.feels_like + 'º');
+    $desc.text('Weather: ' + weatherData.list[0].weather[0].description);
 }
